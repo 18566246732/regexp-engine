422562c5c824bf3f8275d3834bce1b1c4875ef88
Ivan.Liu <Ivan.Liu@fanruan.com>
2020-09-07 16:07:05 +0800

    refactor: clean code

--- example/issue.js
+++
@@ -1,8 +0,0 @@
-/**
- * 替换三目表达式(bad)
- */
-function isAndroid() {
-    const userAgent = navigator.userAgent;
-    // return userAgent.indexOf('Android') > -1 || userAgent.indexOf('Adr') > -1;
-    return /(Android|Adr)/i.test(userAgent);
-}
